92523af8c1b98976a7a43be6b6db1d5591463c8b
Ireton <jireton@ussesjireton01.us.deloitte.com>
2018-10-26 12:03:35 -0700

    A bunch of ones and zeros

---
+++ README.md
@@ -0,0 +1,9 @@
+# Install Vapor Toolbox with Homebrew
+* `brew install vapor/tap/vapor`
+# Generate Xcode Project and launch
+* `cd dlsh-api`
+* `vapor xcode -y`
+# Launch API server
+* Scheme: `Run > My Mac`
+* Press Run button (The 'Play' triangle: ►)
+* Server starting on http://localhost:8080
---
+++ Sources/App/routes.swift
@@ -0,0 +1,19 @@
+import Vapor
+
+/// Register your application's routes here.
+public func routes(_ router: Router) throws {
+    router.get { req in
+        return "DLSH api"
+    }
+    
+    // Basic "Hello, world!" example
+    router.get("hello") { req in
+        return "Hello, world!"
+    }
+
+    // Example of configuring a controller
+    let todoController = TodoController()
+    router.get("todos", use: todoController.index)
+    router.post("todos", use: todoController.create)
+    router.delete("todos", Todo.parameter, use: todoController.delete)
+}
